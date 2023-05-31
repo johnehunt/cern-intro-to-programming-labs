89a6a5cbf669e6e1e5e08a65b781cb23edcf3f1d
John Hunt <john@matraining.com>
2023-05-31 18:34:39 +0100

    Tidied example

--- 05-flow-of-control/converter.py
+++ 05-flow-of-control/converter.py
@@ -1,14 +1,19 @@
 distance_in_km_str = input('Please input the distance in kilometres: ')
 
-if distance_in_km_str.isnumeric():
-    distance_in_km = int(distance_in_km_str)
+distance_in_km_str = distance_in_km_str.replace(' ', '')
 
-    if distance_in_km < 1:
-        print('You must enter a positive distance')
-    else:
-        print(f'You entered the distance {distance_in_km} in kilometres')
-        distance_in_miles = distance_in_km * 0.6214
-        print(f'The distance in miles is {distance_in_miles}')
-
+if distance_in_km_str.startswith('-'):
+    print('You must enter a positive distance')
+elif distance_in_km_str.isalpha():
+    print('Cant be letters')
 else:
-    print('Not an integer number')
+    if distance_in_km_str.count('.') < 2 and distance_in_km_str.replace('.','').isnumeric():
+        # Its a floating point number - possibly
+        distance_in_km = float(distance_in_km_str)
+    elif distance_in_km_str.isnumeric():
+        distance_in_km = int(distance_in_km_str)
+
+    print(f'You entered the distance {distance_in_km} in kilometres')
+    distance_in_miles = distance_in_km * 0.6214
+    print(f'The distance in miles is {distance_in_miles:.4f}')
+
