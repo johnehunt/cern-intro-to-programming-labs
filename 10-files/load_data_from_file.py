cf947a8c5b66acab38968291705ba0e2d122215e
John Hunt <john.hunt10@gmail.com>
2023-10-24 10:45:23 +0100

    Updated to align with notes

--- 10-files/load_data_from_file.py
+++ 10-files/load_data_from_file.py
@@ -4,12 +4,11 @@ print('Opening file')
 
 with open('date_file.txt', 'r') as file:
     print('Reading contents)')
-    lines = file.readlines()
-    print('Accessing first line')
-    date_str = lines[0]
-    print('Date as a string:', date_str)
-    datetime_object = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
-    print('Date as an object:', datetime_object)
+    for line in file:
+        date_str = line
+        print('Date as a string:', date_str)
+        datetime_object = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
+        print('Date as an object:', datetime_object)
 
 
 print('Done')
