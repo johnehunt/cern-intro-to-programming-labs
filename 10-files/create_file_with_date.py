70ef9cf7662a9bb0b8e75a480a5da3f715c94864
John Hunt <john.hunt10@gmail.com>
2023-10-24 10:43:29 +0100

    Simplified solution

--- 10-files/create_file_with_date.py
+++ 10-files/create_file_with_date.py
@@ -2,12 +2,9 @@ from datetime import datetime
 
 print('Creating file')
 
-try:
-    with open('date_file.txt', 'w') as file:
-        print('Writing date information to file')
-        todays_date = str(datetime.today())
-        file.write(todays_date)
-except Exception as exp:
-    print(exp)
+with open('date_file.txt', 'w') as file:
+    print('Writing date information to file')
+    todays_date = str(datetime.today())
+    file.write(todays_date)
 
 print('Done')
--- 10-files/load_data_from_file.py
+++ 10-files/load_data_from_file.py
@@ -2,16 +2,14 @@ from datetime import datetime
 
 print('Opening file')
 
-try:
-    with open('date_file.txt', 'r') as file:
-        print('Reading contents)')
-        lines = file.readlines()
-        print('Accessing first line')
-        date_str = lines[0]
-        print('Date as a string:', date_str)
-        datetime_object = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
-        print('Date as an object:', datetime_object)
-except Exception as exp:
-    print(exp)
+with open('date_file.txt', 'r') as file:
+    print('Reading contents)')
+    lines = file.readlines()
+    print('Accessing first line')
+    date_str = lines[0]
+    print('Date as a string:', date_str)
+    datetime_object = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
+    print('Date as an object:', datetime_object)
+
 
 print('Done')
